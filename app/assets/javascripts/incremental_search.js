45045b350f00684cf6119dc876f95db6709de06d
平田裕哉 <yuya.h0623@gmail.com>
2015-11-13 14:27:20 +0900

    fix

--- app/assets/javascripts/incremental_search.js
+++ app/assets/javascripts/incremental_search.js
@@ -1,21 +1,3 @@
 $(window).on('load', function() {
-  var
-  preFunc = null,
-  preInput = '',
-  input = '',
-  ajaxSearch = function(input) {
-    $.ajax({
-      url: "search",
-      type: "GET",
-      data: ("keyword=" + input)
-    });
-  };
-  $('#inc_search').on('keyup', function() {
-    input = $.trim($(this).val());
-    if(preInput !== input){
-      clearTimeout(preFunc);
-      preFunc = setTimeout(ajaxSearch(input), 500);
-    }
-    preInput = input;
-  });
+//ここにコードを記述してください。
 });
